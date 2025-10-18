02400c7191c138a352be5e42e7bea91204caca61
TheRealPad <pierre-alexandre.delgado-arevalo@epitech.eu>
2025-10-18 14:30:37 +0200

    add docker

--- src/server.js
+++ src/server.js
@@ -1,10 +1,25 @@
+require("dotenv").config();
 const express = require("express");
+const basicAuth = require("express-basic-auth");
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
+
+app.use(
+  "/secret",
+  basicAuth({
+    users: { [process.env.USERNAME]: process.env.PASSWORD },
+    challenge: true,
+    unauthorizedResponse: (req) => "Unauthorized",
+  })
+);
 
 app.get("/", (req, res) => {
   res.send("Hello Pad!");
+});
+
+app.get("/secret", (req, res) => {
+  res.send(process.env.SECRET_MESSAGE);
 });
 
 app.listen(port, () => {
